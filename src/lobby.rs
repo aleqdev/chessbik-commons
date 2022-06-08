49e170ed027fe7984d8887eea3e81415009bd9a5
necromfox <necromfox@gmail.com>
2022-06-08 12:01:01 +0300

    hash

--- src/lobby.rs
+++ src/lobby.rs
@@ -1,6 +1,6 @@
 chessbik_derive_wrapper::derive_wrapper!(
     #[derive(
-        Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize,
+        Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, serde::Serialize, serde::Deserialize,
     )]
     pub struct Lobby(String);
 );
