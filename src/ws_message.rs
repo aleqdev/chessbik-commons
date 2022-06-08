5738ceeb2a85630bea04b40bac223c4e076603d1
necromfox <necromfox@gmail.com>
2022-06-08 11:40:27 +0300

    derivies

--- src/ws_message.rs
+++ src/ws_message.rs
@@ -1,6 +1,6 @@
 use crate::Lobby;
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum WsMessage {
     CreateGame,
     CreateGameCallback(Lobby),
