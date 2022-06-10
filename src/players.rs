17de0048629aaef67b33c03fe25ae25dc1a84ab7
necromfox <necromfox@gmail.com>
2022-06-10 16:01:05 +0300

    ext

--- src/players.rs
+++ src/players.rs
@@ -1,6 +1,15 @@
-use crate::Player;
+use crate::{Player, PlayerColor};
 
 pub struct Players {
     pub white: Player,
     pub black: Player
+}
+
+impl Players {
+    pub fn playing(&mut self, color: PlayerColor) -> &mut Player {
+        match color {
+            PlayerColor::WHITE => &mut self.white,
+            PlayerColor::BLACK => &mut self.black,
+        }
+    }
 }
