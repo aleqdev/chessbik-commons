a5debb7a345e50124423f880c8a61a625e5749c4
necromfox <necromfox@gmail.com>
2022-06-10 21:28:48 +0300

    add is owning to engine record

--- src/player_record.rs
+++ src/player_record.rs
@@ -3,6 +3,6 @@ use crate::{IsOwning, OpponentName};
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum PlayerRecord {
     None,
-    Engine,
+    Engine(IsOwning),
     Opponent(OpponentName, IsOwning),
 }
