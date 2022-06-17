b7f606eb06c4eae9c680cfe0098e67d20174ec9e
necromfox <necromfox@gmail.com>
2022-06-17 17:02:32 +0300

    piece_color eq player_color

--- src/player_color.rs
+++ src/player_color.rs
@@ -1,5 +1,16 @@
+use chessbik_board::PieceColor;
+
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum PlayerColor {
     WHITE,
     BLACK
+}
+
+impl PlayerColor {
+    pub fn eq_piece_color(&self, c: PieceColor) -> bool {
+        match self {
+            PlayerColor::WHITE => c == PieceColor::WHITE,
+            PlayerColor::BLACK => c == PieceColor::BLACK,
+        }
+    }
 }
